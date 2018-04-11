140903f55e7605faee4e467fcc1347674ba22283
Rodrigo Alejandro Mendoza Rojas <ramr16@outlook.com>
2018-04-11 13:13:10 -0500

    Agregar archivos de proyecto.

---
+++ BookService.test/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BookService.test.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BookService.test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookService.test.WebApiApplication" Language="C#" %>
